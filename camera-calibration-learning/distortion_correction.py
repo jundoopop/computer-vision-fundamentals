7490bff7ab5d2399aafc9a52b66d2cf6c2ca572d
권준혁 / Kwon Joonhyeok <45958283+jundoopop@users.noreply.github.com>
2024-04-01 21:10:05 +0900

    [Maintenance]  json saving feature
    # calibrate_
    - Exports .json file as a value for distortion correction
    - But it doesn't work yet.
        - It exports well but hard to detect in directory.

---
+++ camera-calibration-learning/distortion_correction.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2 as cv
+
+# The given video and calibration data
+video_file = "C:\\Users\\a\\computer-vision-fundamentals\\camera-calibration-learning\\chessboard5.mp4"
+
+
+K = np.array(
+    [
+        [612.26865008, 0.0, 643.2243679],
+        [0.0, 611.58145865, 363.62122411],
+        [0.0, 0.0, 1.0],
+    ],
+)  # Derived from `calibrate_camera.py`
+dist_coeff = np.array(
+    [-0.03120115, 0.13887876, 0.00036394, -0.00187562, -0.13854699],
+)
+
+# Open a video
+video = cv.VideoCapture(video_file)
+assert video.isOpened(), "Cannot read the given input, " + video_file
+
+# Run distortion correction
+show_rectify = True
+map1, map2 = None, None
+while True:
+    # Read an image from the video
+    valid, img = video.read()
+    if not valid:
+        break
+
+    # Rectify geometric distortion (Alternative: `cv.undistort()`)
+    info = "Original"
+    if show_rectify:
+        if map1 is None or map2 is None:
+            map1, map2 = cv.initUndistortRectifyMap(
+                K, dist_coeff, None, None, (img.shape[1], img.shape[0]), cv.CV_32FC1
+            )
+        img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR)
+        info = "Rectified"
+    cv.putText(img, info, (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
+
+    # Show the image and process the key event
+    cv.imshow("Geometric Distortion Correction", img)
+    key = cv.waitKey(10)
+    if key == ord(" "):  # Space: Pause
+        key = cv.waitKey()
+    if key == 27:  # ESC: Exit
+        break
+    elif key == ord("\t"):  # Tab: Toggle the mode
+        show_rectify = not show_rectify
+
+video.release()
+cv.destroyAllWindows()
